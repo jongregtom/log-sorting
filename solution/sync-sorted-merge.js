39181be45e85ad13954d97b3190dea4958937905
Jonathan Thomas <jonathan@system76-pc.localdomain>
2022-12-20 10:22:04 -0800

    sync sort implementation completed

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,20 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  // Drain all logs
+  let sortedLogSources = []
+  for (let i = 0; i < logSources.length; i++) {
+    let log = logSources[i].pop()
+    sortedLogSources.push(log)
+  }
+
+  // Sort logs chronologically
+  sortedLogSources.sort((a,b) => a.date - b.date);
+
+  // Print all log entries chronologically
+  for (let i = 0; i < sortedLogSources.length; i++) {
+    printer.print(sortedLogSources[i])
+  }
+  printer.done()
   return console.log("Sync sort complete.");
 };
